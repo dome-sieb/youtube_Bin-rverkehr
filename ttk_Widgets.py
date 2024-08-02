c87798b5bcfe4bde3a6e51914c8010b977e13d9e
dome-sieb <sieberdominique10@gmail.com>
2024-08-02 15:12:13 +0200

    Foto Widget eingefügt und größe Angepasst

--- ttk_Widgets.py
+++ ttk_Widgets.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.geometry('500x500')
@@ -8,9 +8,17 @@ root.geometry('500x500')
 label1 = tk.Label(root, text="Hello World", bg="blue")
 label1.pack()
 
-label2 = ttk.Label(root, text="Label2",)
+
+image = Image.open('IMG_0046.PNG').resize((300, 500))   #Foto Image eingefügt und größe angepasst
+photo = ImageTk.PhotoImage(image)
+label2 = tk.Label(root, image=photo)
 label2.pack()
+
+
+
 
 
 root.mainloop()
 #ttk widgets implementiert das Erscheinungsbild der Widgets
+
+#Konfiguration von ttk widgets mit der .configure methode
